f1953865d0c36237037683afaaf63d36ba68c325
Noé Echard <65755297+Th3PaSt3qu3@users.noreply.github.com>
2022-05-27 15:36:35 +0200

    Update begin

--- lab3/src/App.js
+++ lab3/src/App.js
@@ -1,9 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' exact></Route>
+        </Routes>
+      </Router>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ lab3/src/Components/Navbar.js
@@ -0,0 +1,38 @@
+import React, {useState} from 'react'
+import { Link } from 'react-router-dom'
+
+	function Navbar() {
+
+	const {click, setClick} = useState(false);
+	const handleClick = () => setClick(!click);
+	const closeMobileMenu = () => setClick(false);
+
+	return (
+		<>
+			<nav className='navbar'>
+				<div className='navbar-container'>
+					<Link to='/' className='navbar-logo'>
+						The EGUI Blog <i className='fab fa-typo3'></i>
+					</Link>
+					<div className='menu-icon' onClick={handleClick}>
+						<i className={click ? 'fas fa-times' : 'fas fa-bars'}/>
+					</div>
+					<ul className={click ? 'nav-menu active' : 'nav-menu'}>
+						<li className='nav-item'>
+							<Link to='/Login' className='nav-links' onClick={closeMobileMenu}>
+								Login
+							</Link>
+						</li>
+						<li className='nav-item'>
+							<Link to='/SignUp' className='nav-links-mobile' onClick={closeMobileMenu}>
+								Sign Up
+							</Link>
+						</li>
+					</ul>
+				</div>
+			</nav>      
+		</>
+	)
+	}
+
+	export default Navbar
